3f1a3f2f5e838212dcb1eab7971ebf4b942fa737
code <33802433+codeacade@users.noreply.github.com>
2022-08-12 14:09:55 +0100

    var not const for ipware?

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const get_ip = require('ipware')().get_ip  // to get real IP address
+var get_ip = require('ipware')().get_ip  // to get real IP address
 
 var i = 0;
 var timeMin = 0;
